35e2461f17656e280ef98db836470128fc742a40
Qiyuan An <anqiyuanms@hotmail.com>
2018-02-10 21:02:23 -0500

    input is 128*128*1

--- cnn_3D_printing.py
+++ cnn_3D_printing.py
@@ -13,32 +13,34 @@ tf.logging.set_verbosity(tf.logging.INFO)
 def cnn_model_fn(features, labels, mode):
     """Model function for CNN."""
     # Input layer
-    input_layer = tf.reshape(features["x"], [-1, 28, 28, 1])
+    input_layer = tf.reshape(features["x"], [-1, 128, 128, 1])
 
     # Convolutional layer #1
     conv1 = tf.layers.conv2d(
         inputs=input_layer,
         filters=32,
         kernel_size=[5, 5],
-        padding="same",
+        strides=3,
+        padding='valid',
         activation=tf.nn.relu
     )
 
     # Pooling layer #1
-    pool1 = tf.layers.max_pooling2d(inputs=conv1, pool_size=[2, 2], strides=2)
+    pool1 = tf.layers.max_pooling2d(inputs=conv1, pool_size=[3, 3], strides=3)
 
     # Convolutional layer #2 and pooling layer #2
     conv2 = tf.layers.conv2d(
         inputs=pool1,
         filters=64,
+        strides=3,
         kernel_size=[5, 5],
-        padding="same",
+        padding='same',
         activation=tf.nn.relu
     )
-    pool2 = tf.layers.max_pooling2d(inputs=conv2, pool_size=[2, 2], strides=2)
+    pool2 = tf.layers.max_pooling2d(inputs=conv2, pool_size=[3, 3], strides=3)
 
     # Dense layer
-    pool2_flat = tf.reshape(pool2, [-1, 7 * 7 * 64])
+    pool2_flat = tf.reshape(pool2, [-1, 1 * 1 * 64])
     dense = tf.layers.dense(inputs=pool2_flat, units=1024, activation=tf.nn.relu)
     dropout = tf.layers.dropout(inputs=dense, rate=0.4, training=mode == tf.estimator.ModeKeys.TRAIN)
 
