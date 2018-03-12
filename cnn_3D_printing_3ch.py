a988fd5db5184b2f6a64fe598f3a3348c515b35d
Qiyuan An <anqiyuanms@hotmail.com>
2018-03-12 15:44:25 -0400

    input is 128*128*3

--- cnn_3D_printing_3ch.py
+++ cnn_3D_printing_3ch.py
@@ -1,6 +1,7 @@
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
+import os
 
 # Imports
 import numpy as np
@@ -8,12 +9,79 @@ import tensorflow as tf
 
 tf.logging.set_verbosity(tf.logging.INFO)
 
+IMAGE_SIZE = 128
 
-# our application logic will be added here
+
+# image object from protobuf
+class ImageObject:
+    def __init__(self):
+        self.image = tf.Variable([], dtype=tf.string)
+        self.height = tf.Variable([], dtype=tf.int64)
+        self.width = tf.Variable([], dtype=tf.int64)
+        self.filename = tf.Variable([], dtype=tf.string)
+        self.label = tf.Variable([], dtype=tf.int64)
+
+
+def read_and_decode(filename_queue):
+    imgs = []
+    lbls = []
+    reader = tf.TFRecordReader()
+    _, serialized_example = reader.read(filename_queue)
+    features = tf.parse_single_example(serialized_example, features={
+        "image/encoded": tf.FixedLenFeature([], tf.string),
+        "image/height": tf.FixedLenFeature([], tf.int64),
+        "image/width": tf.FixedLenFeature([], tf.int64),
+        "image/filename": tf.FixedLenFeature([], tf.string),
+        "image/class/label": tf.FixedLenFeature([], tf.int64)})
+    image_encoded = features["image/encoded"]
+    image_raw = tf.image.decode_jpeg(image_encoded, channels=3)
+    image_object = ImageObject()
+    image_object.image = tf.image.resize_image_with_crop_or_pad(image_raw, IMAGE_SIZE, IMAGE_SIZE)
+    image_object.height = features["image/height"]
+    image_object.width = features["image/width"]
+    image_object.filename = features["image/filename"]
+    image_object.label = tf.cast(features["image/class/label"], tf.int64)
+
+    with tf.Session() as sess:
+        # 启动多线程
+        sess.run(tf.local_variables_initializer())
+        coord = tf.train.Coordinator()
+        threads = tf.train.start_queue_runners(sess=sess, coord=coord)
+        while not coord.should_stop():
+            try:
+                img, label = sess.run([image_object.image, image_object.label])
+            except tf.errors.OutOfRangeError:
+                print("Turn to next folder.")
+                break
+            imgs.append(img)
+            lbls.append(label)
+
+        coord.request_stop()
+        coord.join(threads)
+    return imgs, lbls
+
+
+def read_data():
+    # Load training and eval data
+    mnist = tf.contrib.learn.datasets.load_dataset("mnist")
+    train_data = mnist.train.images  # Returns np.array
+    train_labels = np.asarray(mnist.train.labels, dtype=np.int32)
+    eval_data = mnist.test.images  # Returns np.array
+    eval_labels = np.asarray(mnist.test.labels, dtype=np.int32)
+    return train_data, train_labels, eval_data, eval_labels
+
+
+def read_tfrecords(filename):
+    filename_queue = tf.train.string_input_producer([filename])
+    imgs, lbls = read_and_decode(filename_queue)
+
+    return np.array(imgs), np.array(lbls)
+
+
 def cnn_model_fn(features, labels, mode):
     """Model function for CNN."""
     # Input layer
-    input_layer = tf.reshape(features["x"], [-1, 128, 128, 1])
+    input_layer = tf.reshape(features["x"], [-1, 128, 128, 3])
 
     # Convolutional layer #1
     conv1 = tf.layers.conv2d(
@@ -21,7 +89,7 @@ def cnn_model_fn(features, labels, mode):
         filters=32,
         kernel_size=[5, 5],
         strides=2,
-        padding='same',
+        padding='valid',
         activation=tf.nn.relu
     )
 
@@ -79,16 +147,13 @@ def cnn_model_fn(features, labels, mode):
 
 
 def main(unused_argv):
-    # Load training and eval data
-    mnist = tf.contrib.learn.datasets.load_dataset("mnist")
-    train_data = mnist.train.images  # Returns np.array
-    train_labels = np.asarray(mnist.train.labels, dtype=np.int32)
-    eval_data = mnist.test.images  # Returns np.array
-    eval_labels = np.asarray(mnist.test.labels, dtype=np.int32)
-
+    train_tfrecord = './dataset-128/train-00000-of-00001.tfrecord'
+    eval_tfrecord = './dataset-128/validation-00000-of-00001.tfrecord'
+    train_data, train_labels = read_tfrecords(train_tfrecord)
+    eval_data, eval_labels = read_tfrecords(eval_tfrecord)
     # Create the estimator
     mnist_classifier = tf.estimator.Estimator(
-        model_fn=cnn_model_fn, model_dir="./model/"
+        model_fn=cnn_model_fn, model_dir="./model-3ch/"
     )
 
     # Set up logging for predictions
